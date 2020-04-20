ef760564ecc4e384ae3fc230701b6c4acc4f98ec
Alexis Tahakdjian <alexis.tahakdjian@gmail.com>
2020-04-20 17:13:55 +0200

    Modification de la requette Axios pour récupérer l'erreur.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            city: '',
+            city: 'Londres',
+            data: null,
             main: null,
             weather: null,
             fetching: false,
@@ -37,28 +38,34 @@ export default class App extends React.Component {
         e.preventDefault()
         axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&units=metric&lang=fr&appid=634562b88bf8f20cb2bef0366da05683`)
             .then((response) => {
-                    console.log(response.data.cod);
-                    response ?
-                        this.setState({
-                                main: response.data.main,
-                                weather: response.data.weather,
+                    console.log(response.data.cod); //200 pour réussi et 404 pour une erreur
+                    {
+                        response.data.cod === 200 ?
+                            this.setState({
+                                    data: response.data,
+                                    main: response.data.main,
+                                    weather: response.data.weather,
+                                    fetched: true,
+                                    fetching: false,
+                                }
+                            )
+                            : response.data.cod === 404 ?
+                            this.setState({
+                                error: response.data,
                                 fetched: true,
-                            }
-                        )
-                        :
-                        this.setState({
-                            fetching: true,
-                        })
+                                fetching: false,
 
+                            })
+                            :
+                            this.setState({
+                                fetching: true,
+                                fetched: false
+                            })
+                    }
+                    console.log(this.state)
                 }
+            )
 
-            ).catch(error => {
-            console.log(error.response.data.errors);
-            this.setState({
-                fetched: true,
-                error: error
-            });
-        })
     }
 
     icon() {
@@ -117,30 +124,34 @@ export default class App extends React.Component {
                             <CircularProgress/>
                         </Box>
                         :
-                        this.state.fetched === true ?
-                            <Box mt={3} display="flex" justifyContent="space-around" alignItems="center">
-                                <Box display='flex' flexDirection="column" justifyContent="center"
-                                     alignItems="center">
-                                    {this.icon()}
-                                    <Typography>{this.state.weather[0].description}</Typography>
-                                </Box>
-                                <Box>
-                                    <Typography variant="h4">{Math.round(this.state.main.temp)}°C</Typography>
-                                    <Typography>Min : {Math.round(this.state.main.temp_min)}°C</Typography>
-                                    <Typography>Max : {Math.round(this.state.main.temp_max)}°C</Typography>
-                                    <Typography>Ressentie : {Math.round(this.state.main.feels_like)}°C</Typography>
+                        this.state.fetched === true && this.state.data.cod === 200 ?
+                            <Box fullWidth mt={3} display="flex" flexDirection="column" justifyContent="center" alignItems="center" >
+                                <Typography
+                                    variant="h4">{this.state.data.name}, {this.state.data.sys.country}</Typography>
+                                <Box mt={1} width={1} display="flex" justifyContent="space-around" alignItems="center">
+                                    <Box display='flex' flexDirection="column" justifyContent="center"
+                                         alignItems="center">
+
+                                        {this.icon()}
+                                        <Typography>{this.state.weather[0].description}</Typography>
+                                    </Box>
+                                    <Box>
+                                        <Typography variant="h4">{Math.round(this.state.main.temp)}°C</Typography>
+                                        <Typography>Min : {Math.round(this.state.main.temp_min)}°C</Typography>
+                                        <Typography>Max : {Math.round(this.state.main.temp_max)}°C</Typography>
+                                        <Typography>Ressentie : {Math.round(this.state.main.feels_like)}°C</Typography>
+                                    </Box>
                                 </Box>
                             </Box>
-                            :
-                            this.state.error === true && this.state.fetched === true ?
+                                :
+                                this.state.error && this.state.data.cod === 404 ?
                                 <Box mt={3} display="flex" justifyContent="center">
                                     <Typography>Erreur : {this.state.error.message}</Typography>
-                                    <Typography>Erreur : {this.state.error}</Typography>
                                 </Box>
-                            :
-                            <Box mt={3} display="flex" justifyContent="center">
-                                {/*<CircularProgress/>*/}
-                            </Box>
+                                :
+                                <Box mt={3} display="flex" justifyContent="center">
+                                </Box>
+
 
                     }
 
